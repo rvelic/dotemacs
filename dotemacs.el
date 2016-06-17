795c73cfc757b0d6e15e1458ae3c16ed3ce8e0b4
Roman <rvelic@users.noreply.github.com>
2016-06-17 21:50:31 +0200

    Create dotemacs.el

---
+++ dotemacs.el
@@ -0,0 +1,29 @@
+(require 'package)
+(add-to-list 'package-archives'("melpa" . "https://melpa.org/packages/"))
+(package-initialize)
+
+(setq browse-url-browser-function 'browse-url-generic
+      browse-url-generic-program "google-chrome") 
+
+(menu-bar-mode -1)
+(tool-bar-mode -1)
+(scroll-bar-mode -1)
+(toggle-frame-fullscreen)
+
+(setq-default auto-save-default nil)
+(setq make-backup-files nil) 
+
+(fset 'yes-or-no-p 'y-or-n-p)
+
+(custom-set-variables
+ '(inhibit-startup-screen t)
+ '(magit-fetch-arguments (quote ("--prune"))))
+(custom-set-faces)
+(set-face-attribute 'default' nil :height 130)
+
+(load-theme 'wombat t)
+
+(require 'server)
+(server-start)
+
+(load "~/.emacs.d/.emacs-engineer.el")
